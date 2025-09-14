26c284381776034f9aef9203b231eff85d0883c5
ajaykumar1877 <144783450+ajaykumar1877@users.noreply.github.com>
2025-09-14 21:27:36 +0530

    Add files via upload

---
+++ Face Recognition.py
@@ -0,0 +1,44 @@
+# face_prediction
+import face_recognition
+import cv2
+import os
+
+def read_img(path):
+    img = cv2.imread(path)
+    (h, w) = img.shape[:2]
+    width = 500
+    ratio = width / float(w)
+    height = int(h * ratio)
+    return cv2.resize(img, (width, height))
+
+# Store encodings of known faces
+known_encodings = []
+known_names = []
+known_dir = 'known'
+
+for file in os.listdir(known_dir):
+    img = read_img(os.path.join(known_dir, file))
+    img_enc = face_recognition.face_encodings(img)[0]
+    known_encodings.append(img_enc)
+    known_names.append(file.split('.')[0])
+
+# Process unknown faces
+unknown_dir = 'unknown'
+for file in os.listdir(unknown_dir):
+    print("Processing", file)
+    img = read_img(os.path.join(unknown_dir, file))
+    img_enc = face_recognition.face_encodings(img)[0]
+
+    results = face_recognition.compare_faces(known_encodings, img_enc)
+
+    for i in range(len(results)):
+        if results[i]:
+            name = known_names[i]
+            (top, right, bottom, left) = face_recognition.face_locations(img)[0]
+            cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.putText(img, name, (left+2, bottom+20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+
+            # Show output (works in VS Code instead of cv2_imshow)
+            cv2.imshow("Result", img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
